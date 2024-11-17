4c0a5b440a28faf00d726e16bee9283fbfda656d
yokwejuste <yokwejuste@gmail.com>
2024-11-17 17:10:20 +0000

    feat: updates to code

--- passkeyer/settings.py
+++ passkeyer/settings.py
@@ -120,13 +120,10 @@ LOGIN_URL = '/login/'
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-if DEBUG:
-    STATICFILES_DIRS = [
-        BASE_DIR / "static"
-    ]
-else:
-    STATIC_ROOT = BASE_DIR / "static"
-
+# STATIC_ROOT = BASE_DIR / "static"
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
