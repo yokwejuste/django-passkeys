31189067df9af04f60271df9392d5975da54d68c
yokwejuste <yokwejuste@gmail.com>
2024-11-17 07:29:37 +0000

    fix: fixing a few bugs

--- passkeyer/settings.py
+++ passkeyer/settings.py
@@ -131,6 +131,9 @@ LOGIN_URL = '/login/'
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / "static"
+STATICFILES_DIRS = [
+    BASE_DIR / "staticfiles"
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
