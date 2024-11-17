d59a455e58b8891fe86ebdfa8756457f0cbe5e56
yokwejuste <yokwejuste@gmail.com>
2024-11-17 18:08:42 +0000

    updating static location

--- passkeyer/settings.py
+++ passkeyer/settings.py
@@ -119,7 +119,7 @@ LOGIN_URL = ' /login/'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / "static"
 STATICFILES_DIRS = [
     BASE_DIR / "staticfiles",
