ec802f258af4e483b273332a66770d7ea05fc050
yokwejuste <yokwejuste@gmail.com>
2024-11-17 16:59:23 +0000

    feat: updates to code

--- fido_auth/urls.py
+++ fido_auth/urls.py
@@ -4,8 +4,8 @@ from .views import login_view, register_passkey, logout_view, index, create_user
 
 urlpatterns = [
     path('', index, name='index'),
-    path('auth/login/', login_view, name='login'),
-    path('auth/register-passkey/', register_passkey, name='register_passkey'),
-    path('auth/logout/', logout_view, name='logout'),
-    path("auth/register/", create_user_account, name="register_user"),
+    path('login/', login_view, name='login'),
+    path('register-passkey/', register_passkey, name='register_passkey'),
+    path('logout/', logout_view, name='logout'),
+    path("register/", create_user_account, name="register_user"),
 ]
